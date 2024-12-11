9c410568def7adb5d9cae98d512d91ebaef5d19c
Launch Lee <lee2487596275@outlook.com>
2024-12-11 23:14:49 +0800

    Chore: Version numbers

--- danbooru.js
+++ danbooru.js
@@ -2,7 +2,7 @@
 // @name         Danbooru Download Options
 // @namespace    http://tampermonkey.net/
 // @updateURL    https://raw.githubusercontent.com/LaunchLee/danbooru-download-options/refs/heads/main/danbooru.js
-// @version      2024-12-11
+// @version      2024-12-11-001
 // @description  Download images with my name patterns.
 // @author       Launch Lee
 // @match        https://danbooru.donmai.us/*
